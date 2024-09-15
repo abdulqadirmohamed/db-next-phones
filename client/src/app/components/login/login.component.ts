9700bc15913a516d9aa164cbf17ce2238e3e790d
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-09-15 13:50:01 +0300

    auth

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,17 +1,36 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component} from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, NgClass],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   isLoginMode = false;
 
+  email: string = '';
+  password: string = '';
+
+  authForm: FormGroup;
+
+  constructor(private fb:FormBuilder){
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.min(6)]]
+    });
+  }
+
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode
+  }
+
+  onSubmit(){
+    console.log(this.authForm.value)
+    this.authForm.reset()
   }
 }
