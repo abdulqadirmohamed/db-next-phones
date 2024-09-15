9386f0359607d453ce76d95eb75741b52d5673c5
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-09-15 11:18:06 +0300

    auth

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,53 +1,17 @@
-import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { AuthService } from '../../services/auth/auth.service';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  isLoginMode = false;
 
-  credentials = {
-    email: '',
-    password: '',
-  };
-  errorMessage: string = '';
-
-  http = inject(HttpClient)
-  router = inject(Router)
-
-  authSercies = inject(AuthService)
-
-
-
-  // onSubmit(){
-  //   this.http.post("http://localhost:3000/api/auth/sign-in", this.loginObj).subscribe((res:any)=>{
-  //     if(res.data){
-  //       localStorage.setItem('token', res.data.token)
-  //       this.router.navigate(['dashboard'])
-  //     }else{
-  //       alert('error')
-  //     }
-  //   })
-  // }
-
-  onSubmit(){
-    if (!this.credentials.email || !this.credentials.password) {
-      this.errorMessage = 'Username and password are required!';
-      return;  // Stop if either field is empty
-    }
-
-    this.authSercies.login(this.credentials).subscribe({
-      next:(response)=>{
-          localStorage.setItem('token', response.token)
-          this.router.navigate(['/dashboard'])
-      },error: (err) => console.error(err),
-    })
+  onSwitchMode(){
+    this.isLoginMode = !this.isLoginMode
   }
 }
--- client/src/app/services/auth/auth.service.ts
+++ client/src/app/services/auth/auth.service.ts
@@ -1,41 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:3000/api/auth/sign-in'
+  constructor() { }
 
-  constructor(private http: HttpClient, private router: Router) { }
-
-  login(credentials: any): Observable<any> {
-    return this.http.post(this.apiUrl, credentials).pipe(
-      tap((response: any) => {
-        const token = response.token
-        if (token) {
-          localStorage.setItem('token', token);  // Store the token in localStorage
-        } else {
-          console.error('No token received from backend');
-        }
-      })
-    )
-  }
-
-  logout() {
-    localStorage.removeItem('token');
-    this.router.navigate(['/login']);
-  }
-
-  isLoggedIn(): boolean {
-    return !!localStorage.getItem('token');
-  }
-
-  getToken() {
-    return localStorage.getItem('token');
-  }
 
 }
