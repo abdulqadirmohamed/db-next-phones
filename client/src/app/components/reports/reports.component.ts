03d4cbc5efec0bb32a1487767223a7366ec7f34f
abdulqadirmohamed <cajiibxaaji02@gmail.com>
2024-09-24 07:29:50 +0300

    sales section

--- client/src/app/components/reports/reports.component.ts
+++ client/src/app/components/reports/reports.component.ts
@@ -3,16 +3,18 @@ import { Customers } from '../../model/customers';
 import { CustomersService } from '../../services/customers/customers.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, CalendarModule, FormsModule],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css'
 })
 export class ReportsComponent implements OnInit {
   itemList: Customers[] = [];
+  date2: Date | undefined;
 
 
   customerServices = inject(CustomersService);
--- client/src/app/forms/sales-form/sales-form.component.ts
+++ client/src/app/forms/sales-form/sales-form.component.ts
@@ -1,27 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { DropdownModule } from 'primeng/dropdown';
+import { Product } from '../../model/product';
+import { ProductService } from '../../services/product/product.service';
+import { Customers } from '../../model/customers';
+import { CustomersService } from '../../services/customers/customers.service';
+import { CalendarModule } from 'primeng/calendar';
 
 
 @Component({
   selector: 'app-sales-form',
   standalone: true,
-  imports: [FormsModule, DropdownModule],
+  imports: [FormsModule, DropdownModule, CalendarModule],
   templateUrl: './sales-form.component.html',
   styleUrl: './sales-form.component.css'
 })
 export class SalesFormComponent implements OnInit  {
- 
-  products: any[] | undefined;
 
-    selectedProduct: string | undefined;
+  productList: Product[] = []
+  selectedProduct: string | undefined;
+
+  customerList: Customers[] = []
+  selectedCustomer: string | undefined;
+
+  date2: Date | undefined;
+
+  productService = inject(ProductService)
+  customerService = inject(CustomersService)
 
     ngOnInit() {
-        this.products = [
-            { name: 'Iphone 11'},
-            { name: 'Keyboard'},
-            { name: 'Charger'},
-            { name: 'Earophone'},
-        ];
+      this.productService.getAllItems().subscribe((result:any)=>{
+        this.productList = result.data;
+      })
+
+      this.customerService.getAllItems().subscribe((result:any)=>{
+        this.customerList = result.data
+      })
+
+
     }
 }
